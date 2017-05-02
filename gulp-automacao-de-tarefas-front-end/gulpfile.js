351b886e9863b13c96fa63eedefa453f39fb70f3
Bruno Naibert <brunonaibert@hotmail.com>
2017-05-01 22:30:33 -0300

    Lidando com latência e banda 4

--- gulp-automacao-de-tarefas-front-end/gulpfile.js
+++ gulp-automacao-de-tarefas-front-end/gulpfile.js
@@ -2,7 +2,8 @@ var gulp = require('gulp'),
     imagemin = require('gulp-imagemin'),
     clean = require('gulp-clean'),
     concat = require('gulp-concat'),
-    htmlReplace = require('gulp-html-replace');
+    htmlReplace = require('gulp-html-replace'),
+    uglify = require('gulp-uglify');
 
 gulp.task('default', ['copy'], function() {
 
@@ -32,6 +33,7 @@ gulp.task('build-js', function() {
 
   gulp.src(['dist/js/jquery.js', 'dist/js/home.js', 'dist/js/produto.js'])
     .pipe(concat('all.js'))
+    .pipe(uglify())
     .pipe(gulp.dest('dist/js'));
 });
 
