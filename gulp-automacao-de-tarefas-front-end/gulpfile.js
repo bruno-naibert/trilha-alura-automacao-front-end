5f36e75e863bc9c13e2d02bea335f35bad976a71
Bruno Naibert <brunonaibert@hotmail.com>
2017-05-01 22:54:17 -0300

    Lidando com latência e banda 5

--- gulp-automacao-de-tarefas-front-end/gulpfile.js
+++ gulp-automacao-de-tarefas-front-end/gulpfile.js
@@ -4,11 +4,12 @@ var gulp = require('gulp'),
     concat = require('gulp-concat'),
     htmlReplace = require('gulp-html-replace'),
     uglify = require('gulp-uglify'),
-    usemin = require('gulp-usemin');
+    usemin = require('gulp-usemin'),
+    cssmin = require('gulp-cssmin');
 
 gulp.task('default', ['copy'], function() {
 
-  gulp.start('build-img', 'build-js', 'build-html');
+  gulp.start('build-img', 'usemin');
 });
 
 gulp.task('copy', ['clean'], function() {
@@ -30,19 +31,12 @@ gulp.task('build-img', function() {
     .pipe(gulp.dest('dist/img'));
 });
 
-gulp.task('build-js', function() {
-
-  gulp.src(['dist/js/jquery.js', 'dist/js/home.js', 'dist/js/produto.js'])
-    .pipe(concat('all.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest('dist/js'));
-});
-
-gulp.task('build-html', function() {
+gulp.task('usemin', function() {
 
   gulp.src('dist/**/*.html')
-    .pipe(htmlReplace({
-      js: 'js/all.js'
+    .pipe(usemin({
+      'js' : [uglify],
+      'css' : [cssmin]
     }))
     .pipe(gulp.dest('dist'));
 });
